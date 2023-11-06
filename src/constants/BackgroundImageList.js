c5a888e0e5c5e0393cb1d56a81cc2c250f22bad9
euijunh <euijunheo@naver.com>
2023-11-06 13:57:24 +0900

    added Background image list

---
+++ src/constants/BackgroundImageList.js
@@ -0,0 +1,14 @@
+const BACKGROUND_IMAGE_LIST = [
+  "https://raw.githubusercontent.com/euijunh/momentum/main/img/img01.jpg",
+  "https://raw.githubusercontent.com/euijunh/momentum/main/img/img02.jpg",
+  "https://raw.githubusercontent.com/euijunh/momentum/main/img/img03.jpg",
+  "https://raw.githubusercontent.com/euijunh/momentum/main/img/img04.jpg",
+  "https://raw.githubusercontent.com/euijunh/momentum/main/img/img05.jpg",
+  "https://raw.githubusercontent.com/euijunh/momentum/main/img/img06.jpg",
+  "https://raw.githubusercontent.com/euijunh/momentum/main/img/img07.jpg",
+  "https://raw.githubusercontent.com/euijunh/momentum/main/img/img08.jpg",
+  "https://raw.githubusercontent.com/euijunh/momentum/main/img/img09.jpg",
+  "https://raw.githubusercontent.com/euijunh/momentum/main/img/img10.jpg",
+]
+
+export default BACKGROUND_IMAGE_LIST
